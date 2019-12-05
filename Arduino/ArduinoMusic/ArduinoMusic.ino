0107880d4b8d00dc206f0892ab2736798f406627
Xiaole Tao <xiaolet@cisco.com>
2019-12-05 11:58:19 +0800

    virtual code

---
+++ Arduino/ArduinoMusic/ArduinoMusic.ino
@@ -0,0 +1,42 @@
+include "tonedict.h"
+
+
+int tonePin = 11; //蜂鸣器pin, 支持PWM
+int senserPin = 24; //传感器pin, 支持analog输入 
+
+// library 是个结构体
+// library = [
+//   { // music 1
+//     speed: 120,
+//     tones: [[声调,节拍],[声调,节拍],[声调,节拍],[声调,节拍],[声调,节拍],[声调,节拍]]
+//   },
+//   { // music 2
+//     speed: 120,
+//     tones: [[声调,节拍],[声调,节拍],[声调,节拍],[声调,节拍],[声调,节拍],[声调,节拍]]
+//   }
+// ];
+float library[][][] = {
+  {
+    {声调,节拍}, {声调,节拍}, {声调,节拍}, {声调,节拍}, {声调,节拍}, {声调,节拍}, {声调,节拍}
+  }
+}
+
+void setup()
+{
+  pinMode(tonePin, OUTPUT); 
+}
+
+void loop()
+{
+  if (等待传感器事件) {
+    play(libiary[随机数]);
+  }
+}
+
+void play(music)
+{
+  float ratio = 60*1000/speed;  //每一拍的基准时间
+  for i = 遍历(music) {
+    tone(tonePin, music[i][0], music[i][1]*ratio); 
+  }
+}
---
+++ Arduino/ArduinoMusic/tonedict.h
@@ -0,0 +1,57 @@
+#ifndef TONE_DEFINE
+#define TONE_DEFINE
+//定义频率表
+#define STOP 0     // 休止符
+#define T1L 262    // 低音1
+#define T2L 294    // 低音2
+#define T3L 330    // 低音3
+#define T4L 349    // 低音4
+#define T5L 392    // 低音5
+#define T6L 440    // 低音6
+#define T7L 494    // 低音7
+#define T1 523     // 中音1
+#define T2 578     // 中音2
+#define T3 659     // 中音3
+#define T4 698     // 中音4
+#define T5 784     // 中音5
+#define T6 880     // 中音6
+#define T7 988     // 中音7
+#define T1H 1046   // 高音1
+#define T2H 1175   // 高音2
+#define T3H 1318   // 高音3
+#define T4H 1397   // 高音4
+#define T5H 1568   // 高音5
+#define T6H 1760   // 高音6
+#define T7H 1976   // 高音7
+#define T1LP 277   // 低音1#
+#define T2LP 311   // 低音2#
+#define T3LP 340   // 低音3#
+#define T4LP 370   // 低音4#
+#define T5LP 415   // 低音5#
+#define T6LP 466   // 低音6#
+#define T7LP 524   // 低音7#
+#define T1P 554    // 中音1#
+#define T2P 622    // 中音2#
+#define T3P 682    // 中音3#
+#define T4P 740    // 中音4#
+#define T5P 831    // 中音5#
+#define T6P 932    // 中音6#
+#define T7P 1046   // 中音7#
+#define T1HP 1109  // 高音1#
+#define T2HP 1245  // 高音2#
+#define T3HP 1356  // 高音3#
+#define T4HP 1480  // 高音4#
+#define T5HP 1661  // 高音5#
+#define T6HP 1865  // 高音6#
+#define T7HP 2066  // 高音7#
+
+#define C 1         // 1拍
+#define CC 1        // 2拍
+#define CCC 1       // 3拍
+#define CCCC 1      // 4拍
+#define C2 0.5      // 1/2拍
+#define C4 0.25     // 1/4拍
+#define C8 0.125    // 1/8拍
+#define C16 0.0625  // 1/16拍
+
+#endif
