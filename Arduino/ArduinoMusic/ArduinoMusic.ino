6632549b35512f3e8e408ca0f3503f8cda386465
Xiaole Tao <xiaolet@cisco.com>
2019-12-08 17:23:25 +0800

    update Code

--- Arduino/ArduinoMusic/ArduinoMusic.ino
+++ Arduino/ArduinoMusic/ArduinoMusic.ino
@@ -1,27 +1,40 @@
-#include "tonedict.h"
+#include "Tonelib.h"  //LED接线查看Tonelob.h文件的port函数
 
-int tonePin = A0;
+int tonePin = A2;  //喇叭接一头接GND，另一头接tonePin
 
-//乒乓变奏曲
-String aa = "331122x 1x13212 331122x 1x13231";
-String bb = "2222221 2222221 2222221 2222221";
+String music[] = {  //每三行为一曲, 速度，音调，节拍
+
+  "160", //Jingle Bell
+  "x321x0 x321y0y y4325555 654210 333333 351230 444443333 322125 333333 351230 444443333 554210",
+  "222211 2222122 22222222 2222a2 221221 22b3a2 22b322233 222211 221221 22b3a2 22b322233 2222a2",
+
+  "120",  //乒乓变奏曲
+  "331122x 1x13212 331122x 1x13231",
+  "2222221 2222221 2222221 2222221",
+
+  "120",  //国歌
+  "0x 11 11xyz 11 03123 55 3313 532 2 65 23 5305 3231 30 xy11 3355 222y 2x 11 33 5 1355 65 3155 3010 x1 3155 3010 x1 x1 x1 10",
+  "22 a2 b3233 11 22233 11 b3b3 b31 z 11 11 2222 2331 11 b322 b322 2331 a2 a2 a2 z b322 11 b322 2222 11 b322 2222 11 11 11 11"
+};
 
 void setup() {
   pinMode(tonePin, OUTPUT);
-  Serial.begin(9600);
 }
 
 void loop() {
-  play(aa, bb, 120);
-  noTone(tonePin);
-  delay(5000);
+  play(1);
 }
 
-void play(String song, String pace, int speed) {
+void play(int idx) {
+  int baseIndex = (idx - 1) * 3;
+  int speed = music[baseIndex].toInt();
+  String song = music[baseIndex + 1];
+  String pace = music[baseIndex + 2];
   for (int i = 0; i < song.length(); i++) {
-    tone(tonePin, melody(song[i]), duration(pace[i], speed));
     digitalWrite(port(song[i]), HIGH);
+    tone(tonePin, melody(song[i]), duration(pace[i], speed));
     delay(duration(pace[i], speed) + 10);
     digitalWrite(port(song[i]), LOW);
   }
+  noTone(tonePin);
 }
---
+++ Arduino/ArduinoMusic/Tonelib.h
@@ -0,0 +1,53 @@
+#ifndef TONELIB_DEFINE
+#define TONELIB_DEFINE
+
+int melody(char s) {  // 频率
+    if      (s == 'w') { return 349; }   // 低音4#
+    else if (s == 'x') { return 392; }   // 低音5#
+    else if (s == 'y') { return 440; }   // 低音6#
+    else if (s == 'z') { return 494; }   // 低音7#
+    else if (s == '1') { return 523; }   // 中音1#
+    else if (s == '2') { return 578; }   // 中音2#
+    else if (s == '3') { return 659; }   // 中音3#
+    else if (s == '4') { return 698; }   // 中音4#
+    else if (s == '5') { return 784; }   // 中音5#
+    else if (s == '6') { return 880; }   // 中音6# 
+    else if (s == '7') { return 988; }   // 中音7#
+    else if (s == 'a') { return 1046; }  // 高音1#
+    else if (s == 'b') { return 1175; }  // 高音2#
+    else if (s == 'c') { return 1318; }  // 高音3#
+    else { return  0; }
+}
+
+int port(char s) { // 发音同时点亮哪个端口的LED
+    if      (s == 'x') { return A0; }  // 低音5#
+    else if (s == 'y') { return 2; }   // 低音6#
+    else if (s == 'z') { return 3; }   // 低音7#
+    else if (s == '1') { return 4; }   // 中音1#
+    else if (s == '2') { return 5; }   // 中音2#
+    else if (s == '3') { return 6; }   // 中音3#
+    else if (s == '4') { return 7; }   // 中音4#
+    else if (s == '5') { return 8; }   // 中音5#
+    else if (s == '6') { return 9; }   // 中音6#
+    else if (s == '7') { return 10; }  // 中音7#
+    else if (s == 'a') { return 11; }  // 高音1#
+    else if (s == 'b') { return 12; }  // 高音2#
+    else { return  0; }
+}
+
+int duration(char s, int speed) {  // 算出节拍时长
+    float unit = 60000 / speed;
+    if      (s == 'x') { return int(unit * 4); }       // 4拍
+    else if (s == 'y') { return int(unit * 3); }       // 3拍
+    else if (s == 'z') { return int(unit * 2); }       // 2拍
+    else if (s == 'a') { return int(unit * 1.5); }     // 1拍 延音
+    else if (s == '1') { return int(unit * 1); }       // 1拍
+    else if (s == 'b') { return int(unit * 0.75); }    // 1/2拍 延音
+    else if (s == '2') { return int(unit * 0.5); }     // 1/2拍
+    else if (s == 'c') { return int(unit * 0.375); }   // 1/4拍 延音
+    else if (s == '3') { return int(unit * 0.25); }    // 1/4拍
+    else if (s == '4') { return int(unit * 0.125); }   // 1/8拍
+    else { return  0; }
+}
+
+#endif
